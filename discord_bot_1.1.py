b553b5f06a446c2f91f88d1ffe7f63e107b4cacf
white201 <75025529+white-201@users.noreply.github.com>
2021-01-13 19:58:16 +0900

    Add files via upload

--- discord_bot_1.1.py
+++ discord_bot_1.1.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import os
 import random
 import openpyxl
@@ -8,36 +9,59 @@ import sys
 import asyncio
 import datetime
 
+
+
 #TOKEN = os.environ['BOT_TOKEN']
-TOKEN = 'Nzk3NzUzODAwOTE1OTQzNDU0.X_rEHw.cMv7n9Q2ioUoaxFaQym-uZs8Hzc'
+temp_TOKEN= 'N/z/k/3/N/z/U/z/O/D/A/w/O/T/E/1/O/T/Q/z/N/D/U/0/./X/_/r/E/H/w/./F/z/T/E/C/A/z/K/D/N/f/d/j/a/0/G/c/s/c/K/Q/H/5/M/6/o/s'
+TOKEN = ''
+for i in range(len(temp_TOKEN)): #디스코드 자동 토큰 짤림 없애주는 코드
+    if i%2==0:
+        TOKEN += temp_TOKEN[i]
+
 client = discord.Client()
 
 @client.event
+
 async def on_message(message):  #봇이 쓰는 명령에는 반응 안되게 하는 부분
     if message.author.bot:
         return None
 
     if message.content.startswith("!명령어"):
-        command_list = ["!강화 !도박", "!순위 !내순위" ,"!내레벨", "!노래","!행운의숫자"]
-        command_msg_list = ["강화를 진행합니다.", "강화 순위를 표시합니다.","내 강화 레벨을 표시합니다.", "추천 노래를 표시합니다.","1부터 99까지 숫자 중 행운의 숫자를 골라줍니다."]
+        command_list = ["!강화 !도박", "!순위 !내순위" ,"!내레벨", "!노래","!행운의숫자", "!개발자명령어"]
+        command_msg_list = ["강화를 진행합니다.", "강화 순위를 표시합니다.","내 강화 레벨을 표시합니다.", "★★★추천 노래를 표시합니다.★★★","1부터 99까지 숫자 중 행운의 숫자를 골라줍니다.","개발자를 위한 명령어들을 표시합니다."]
         embed = discord.Embed(title="명령어",color=0x62c1cc)
         for i in range(len(command_list)):
             embed.add_field(name=command_list[i], value=command_msg_list[i], inline=False)
         await message.channel.send(embed=embed)
-            
 
-        
+    if message.content.startswith("!개발자명령어"):
+        command_list = ["!디렉토리표시"]
+        command_msg_list = ["봇이 실행되고 있는 디렉토리를 표시합니다."]
+        embed = discord.Embed(title="개발자 명령어",color=0x62c1cc)
+        for i in range(len(command_list)):
+            embed.add_field(name=command_list[i], value=command_msg_list[i], inline=False)
+        await message.channel.send(embed=embed)
+       
+
+    if message.content.startswith("!디렉토리확인"):
+        await message.channel.send(os.getcwd())
+        await message.channel.send(os.path.dirname(os.path.realpath(__file__)))
+        await message.channel.send(__file__)
+        await message.channel.send(os.path.realpath(__file__))
+        await message.channel.send(os.path.abspath(__file__))
 
     if message.content.startswith("!강화"):
+        
         now = datetime.datetime.now()
-        print("강화"+str(message.author)+str(now))
+        print("강화" + str(message.author) + str(now))
         exp = random.randrange(-5,15) #몇 레벨을 증가하고 감소할지 정하는 부분
-        if exp >13:
+        if exp > 13:
             exp = random.randrange(13, 30)
+        os.chdir("D:\\3. 코딩\\2. 디스코드 봇\\discord-bot-white201-main\\discord bot 1.1\\discord bot 1.1")
         file = openpyxl.load_workbook("data.xlsx")
         sheet = file.active
 
-        success_failure = random.randrange(0, 100) #강화가 실패할 확률을 정하는 부분    
+        success_failure = random.randrange(0, 100) #강화가 실패할 확률을 정하는 부분
         success_failure_ox = True  #True가 안터진거, False가 터진거
 
         boom = random.randrange(0, 100) #강화가 터질 확률을 정하는 부분
@@ -52,30 +76,30 @@ async def on_message(message):  #봇이 쓰는 명령에는 반응 안되게 하
 
         i = 1
         while True:    #엑셀 파일에 A를 위부터 내려오면서 입력자와 같은지 확인하고, 다르면 아래로 내리고 찾아서 값을 바꿈
-            if sheet["A"+str(i)].value == str(message.author.id):
+            if sheet["A" + str(i)].value == str(message.author.id):
                 past_record_ox = True
                 old_level_th = i
-                old_level = sheet["B"+str(i)].value
-                sheet["C"+str(i)].value = str(message.author)
+                old_level = sheet["B" + str(i)].value
+                sheet["C" + str(i)].value = str(message.author)
                 break
-            if sheet["A"+str(i)].value == None:    #내려오다가 값이 비어있으면 거기에 값을 입력함
+            if sheet["A" + str(i)].value == None:    #내려오다가 값이 비어있으면 거기에 값을 입력함
                 past_record_ox = False
-                sheet["A"+str(i)].value = str(message.author.id)
-                sheet["B"+str(i)].value = exp
-                sheet["C"+str(i)].value = str(message.author)
+                sheet["A" + str(i)].value = str(message.author.id)
+                sheet["B" + str(i)].value = exp
+                sheet["C" + str(i)].value = str(message.author)
                 break
             i+=1
         if past_record_ox == True:
-            if boom > (old_level/100): #터지지 않았을 경우
+            if boom > (old_level / 100): #터지지 않았을 경우
                 boom_ox = False
-                if success_failure > (old_level/10): #강화에 성공했을 경우
+                if success_failure > (old_level / 10): #강화에 성공했을 경우
                     success_failure_ox = True
-                    sheet["B"+str(old_level_th)].value += exp
+                    sheet["B" + str(old_level_th)].value += exp
                     if exp > 0:   #강화를 통해 증가했는지 감소했는지 정하는 부분
                         decrease_increase = "증가량 : +"
                     elif exp <= 0:
                         decrease_increase = "감소량 : "
-                    msg = "<@{}>님 강화 진행! 강화 레벨은 {} 입니다.(기존 레벨 : {}, {}{})".format(message.author.id,sheet["B"+str(old_level_th)].value, old_level,decrease_increase,exp)
+                    msg = "<@{}>님 강화 진행! 강화 레벨은 {} 입니다.(기존 레벨 : {}, {}{})".format(message.author.id,sheet["B" + str(old_level_th)].value, old_level,decrease_increase,exp)
                     await message.channel.send(msg)
                 else: #강화를 실패했을 경우
                     success_failure_ox = False
@@ -83,7 +107,7 @@ async def on_message(message):  #봇이 쓰는 명령에는 반응 안되게 하
                     await message.channel.send(msg)
             else: #터졌을 경우
                 boom_ox = True
-                sheet["B"+str(old_level_th)].value = 0
+                sheet["B" + str(old_level_th)].value = 0
                 msg = "<@{}>님 강화 실패! 아이쿠 터졌습니다. 강화 레벨은 0LV 입니다.".format(str(message.author.id))
                 await message.channel.send(msg)
 
@@ -91,14 +115,15 @@ async def on_message(message):  #봇이 쓰는 명령에는 반응 안되게 하
 
     if message.content.startswith("!도박"):
         now = datetime.datetime.now()
-        print("도박 "+str(message.author)+" "+str(now))
+        print("도박 " + str(message.author) + " " + str(now))
         exp = random.randrange(-5,15) #몇 레벨을 증가하고 감소할지 정하는 부분
-        if exp >13:
+        if exp > 13:
             exp = random.randrange(13, 30)
+        os.chdir("D:\\3. 코딩\\2. 디스코드 봇\\discord-bot-white201-main\\discord bot 1.1\\discord bot 1.1")
         file = openpyxl.load_workbook("data.xlsx")
         sheet = file.active
 
-        success_failure = random.randrange(0, 100) #강화가 실패할 확률을 정하는 부분    
+        success_failure = random.randrange(0, 100) #강화가 실패할 확률을 정하는 부분
         success_failure_ox = True  #True가 안터진거, False가 터진거
 
         boom = random.randrange(0, 100) #강화가 터질 확률을 정하는 부분
@@ -113,30 +138,30 @@ async def on_message(message):  #봇이 쓰는 명령에는 반응 안되게 하
 
         i = 1
         while True:    #엑셀 파일에 A를 위부터 내려오면서 입력자와 같은지 확인하고, 다르면 아래로 내리고 찾아서 값을 바꿈
-            if sheet["A"+str(i)].value == str(message.author.id):
+            if sheet["A" + str(i)].value == str(message.author.id):
                 past_record_ox = True
                 old_level_th = i
-                old_level = sheet["B"+str(i)].value
-                sheet["C"+str(i)].value = str(message.author)
+                old_level = sheet["B" + str(i)].value
+                sheet["C" + str(i)].value = str(message.author)
                 break
-            if sheet["A"+str(i)].value == None:    #내려오다가 값이 비어있으면 거기에 값을 입력함
+            if sheet["A" + str(i)].value == None:    #내려오다가 값이 비어있으면 거기에 값을 입력함
                 past_record_ox = False
-                sheet["A"+str(i)].value = str(message.author.id)
-                sheet["B"+str(i)].value = exp
-                sheet["C"+str(i)].value = str(message.author)
+                sheet["A" + str(i)].value = str(message.author.id)
+                sheet["B" + str(i)].value = exp
+                sheet["C" + str(i)].value = str(message.author)
                 break
             i+=1
         if past_record_ox == True:
-            if boom > (old_level/100): #터지지 않았을 경우
+            if boom > (old_level / 100): #터지지 않았을 경우
                 boom_ox = False
-                if success_failure > (old_level/10): #강화에 성공했을 경우
+                if success_failure > (old_level / 10): #강화에 성공했을 경우
                     success_failure_ox = True
-                    sheet["B"+str(old_level_th)].value += exp
+                    sheet["B" + str(old_level_th)].value += exp
                     if exp > 0:   #강화를 통해 증가했는지 감소했는지 정하는 부분
                         decrease_increase = "증가량 : +"
                     elif exp <= 0:
                         decrease_increase = "감소량 : "
-                    msg = "<@{}>님 강화 진행! 강화 레벨은 {} 입니다.(기존 레벨 : {}, {}{})".format(message.author.id,sheet["B"+str(old_level_th)].value, old_level,decrease_increase,exp)
+                    msg = "<@{}>님 강화 진행! 강화 레벨은 {} 입니다.(기존 레벨 : {}, {}{})".format(message.author.id,sheet["B" + str(old_level_th)].value, old_level,decrease_increase,exp)
                     await message.channel.send(msg)
                 else: #강화를 실패했을 경우
                     success_failure_ox = False
@@ -144,24 +169,25 @@ async def on_message(message):  #봇이 쓰는 명령에는 반응 안되게 하
                     await message.channel.send(msg)
             else: #터졌을 경우
                 boom_ox = True
-                sheet["B"+str(old_level_th)].value = 0
+                sheet["B" + str(old_level_th)].value = 0
                 msg = "<@{}>님 강화 실패! 아이쿠 터졌습니다. 강화 레벨은 0LV 입니다.".format(str(message.author.id))
                 await message.channel.send(msg)
 
         file.save("data.xlsx")
 
     if message.content.startswith("!순위"):
+        os.chdir("D:\\3. 코딩\\2. 디스코드 봇\\discord-bot-white201-main\\discord bot 1.1\\discord bot 1.1")
         file = openpyxl.load_workbook("data.xlsx")
         sheet = file.active
   
-        name_ranking_list=[]
-        point_ranking_list=[]
-        i=1
+        name_ranking_list = []
+        point_ranking_list = []
+        i = 1
         while True:
-            if sheet["A"+str(i)].value == None:
+            if sheet["A" + str(i)].value == None:
                 break
-            name_ranking_list.append(sheet["C"+str(i)].value)
-            point_ranking_list.append(sheet["B"+str(i)].value)
+            name_ranking_list.append(sheet["C" + str(i)].value)
+            point_ranking_list.append(sheet["B" + str(i)].value)
             i+=1
         for i in range(len(point_ranking_list) - 1):
             man_idx = i
@@ -172,41 +198,43 @@ async def on_message(message):  #봇이 쓰는 명령에는 반응 안되게 하
             name_ranking_list[i], name_ranking_list[man_idx] = name_ranking_list[man_idx], name_ranking_list[i]
 
         embed = discord.Embed(title="순위표",color=0x62c1cc)
-        embed.add_field(name="주의사항",value="아직 베타 버전이기에 버그가 있을 수도 있습니다.", inline=False)
+        embed.add_field(name = "주의사항",value="※※※아직 베타 버전이기에 버그가 있을 수도 있습니다.※※※", inline=False)
         for i in range(0,10):
             temp_player = "{}".format(name_ranking_list[i])
-            embed.add_field(name=str(i+1)+"위", value=temp_player+" : "+str(point_ranking_list[i])+"점", inline=False)
-        await message.channel.send(embed=embed)
+            embed.add_field(name=str(i + 1) + "위", value=temp_player + " : " + str(point_ranking_list[i]) + "점", inline=False)
+        await message.channel.send(embed = embed)
 
     if message.content.startswith("!내레벨"):
+        os.chdir("D:\\3. 코딩\\2. 디스코드 봇\\discord-bot-white201-main\\discord bot 1.1\\discord bot 1.1")
         file = openpyxl.load_workbook("data.xlsx")
         sheet = file.active
         temp = 0
         i = 1
         while True:
-            if sheet["A"+str(i)].value == None:
+            if sheet["A" + str(i)].value == None:
                 break
-            if sheet["A"+str(i)].value == str(message.author.id):
+            if sheet["A" + str(i)].value == str(message.author.id):
                 temp = i
                 break
             i += 1
-        msg = "<@{}>님의 레벨은 {}LV 입니다.".format(message.author.id,sheet["B"+str(temp)].value)
+        msg = "<@{}>님의 레벨은 {}LV 입니다.".format(message.author.id,sheet["B" + str(temp)].value)
         await message.channel.send(msg)
         file.close()
 
     if message.content.startswith("!내순위"):
         print("내순위")
+        os.chdir("D:\\3. 코딩\\2. 디스코드 봇\\discord-bot-white201-main\\discord bot 1.1\\discord bot 1.1")
         file = openpyxl.load_workbook("data.xlsx")
         sheet = file.active
-  
-        name_ranking_list=[]
-        point_ranking_list=[]
-        i=1
+
+        name_ranking_list = []
+        point_ranking_list = []
+        i = 1
         while True:
-            if sheet["A"+str(i)].value == None:
+            if sheet["A" + str(i)].value == None:
                 break
-            name_ranking_list.append(sheet["C"+str(i)].value)
-            point_ranking_list.append(sheet["B"+str(i)].value)
+            name_ranking_list.append(sheet["C" + str(i)].value)
+            point_ranking_list.append(sheet["B" + str(i)].value)
             i+=1
         for i in range(len(point_ranking_list) - 1):
             man_idx = i
@@ -221,15 +249,16 @@ async def on_message(message):  #봇이 쓰는 명령에는 반응 안되게 하
             if str(message.author) == name_ranking_list[i]:
                 temp = i
                 break
-        msg = "<@{}>님의 순위는 {}LV로 {}위 입니다. \n 전체 순위를 보고 싶으면 !순위를 입력해주세요.".format(message.author.id,point_ranking_list[temp] ,str(temp+1))
+        msg = "<@{}>님의 순위는 {}LV로 {}위 입니다. \n 전체 순위를 보고 싶으면 !순위를 입력해주세요.".format(message.author.id,point_ranking_list[temp] ,str(temp + 1))
         await message.channel.send(msg)
 
 
+    
         
     if message.content.startswith("!행운의숫자"):
         random_num1 = str(random.randrange(1,99))
-        await message.channel.send("<@{}>님의 행운의 숫자는 "+random_num1+" 입니다").format(message.author.id)
-        print("<@{}>님의 행운의 숫자는 "+random_num1+" 입니다").format(message.author.id)
+        await message.channel.send("<@{}>님의 행운의 숫자는 " + random_num1 + " 입니다").format(message.author.id)
+        print("<@{}>님의 행운의 숫자는 " + random_num1 + " 입니다").format(message.author.id)
 
     if message.content.startswith("@white201#7964"):
         await message.channel.send("뭐이 시끼야!")
@@ -367,7 +396,7 @@ async def on_ready():
     print("bot ID : " + str(client.user.id))
     print("bot verson : " + str(discord.__version__))
     print('------------------')
-    game = discord.Game("white201")
+    game = discord.Game("!명령어")
     await client.change_presence(status=discord.Status.online, activity=game)
 
 client.run(TOKEN)
